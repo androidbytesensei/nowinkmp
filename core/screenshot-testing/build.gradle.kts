709c87365479843c6c7c8ed192d78b3ccac0ff87
Sk Niyaj Ali <niyaj639@gmail.com>
2025-04-09 14:23:11 +0530

    feat: Migrated Screenshot Testing Module to KMP

--- core/screenshot-testing/build.gradle.kts
+++ core/screenshot-testing/build.gradle.kts
@@ -14,20 +14,24 @@
  * limitations under the License.
  */
 plugins {
-    alias(libs.plugins.nowinandroid.android.library)
-    alias(libs.plugins.nowinandroid.android.library.compose)
+    alias(libs.plugins.nowinandroid.kotlin.multiplatform.library)
 }
 
 android {
     namespace = "com.google.samples.apps.nowinandroid.core.screenshottesting"
 }
 
-dependencies {
-    api(libs.bundles.androidx.compose.ui.test)
-    api(libs.roborazzi)
-    api(libs.roborazzi.accessibility.check)
-    implementation(libs.androidx.compose.ui.test)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.robolectric)
-    implementation(projects.core.designsystem)
+kotlin {
+    sourceSets {
+        androidMain.dependencies {
+            api(libs.bundles.androidx.compose.ui.test)
+            api(libs.roborazzi)
+            api(libs.roborazzi.accessibility.check)
+            implementation(libs.androidx.compose.ui.test)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.robolectric)
+            implementation(projects.core.designsystem)
+        }
+    }
 }
+
